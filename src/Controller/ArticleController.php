7478b8e27c2c5efa7bc11fdf76c2a8d0efe69039
SYED Salman <syedsalman8986@gmail.com>
2024-09-26 12:10:53 +0200

    Twig & Controller

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,32 +6,43 @@ use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'app_article')]
-    public function articleAll(): Response
+    public function __construct(private ArticleRepository $articleRepository)
     {
+    }
+
+    #[Route('/articles', name: 'app_article_index')]
+    public function index(Request $request, PaginatorInterface $paginator): Response
+    {
+        $queryBuilder = $this->articleRepository->createQueryBuilder('a')
+            ->orderBy('a.createdAt', 'DESC');
+
+        $pagination = $paginator->paginate(
+            $queryBuilder,
+            $request->query->getInt('page', 1),
+            10 // Nombre d'articles par page
+        );
+
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
-            'articles' => $articles->findBy(
-                    [],
-                    ['createdAt' => 'DESC']
-                )
+            'pagination' => $pagination
         ]);
     }
 
-   #Route : /article/{id}, name="article_show", methods=["GET"])
-   #[Route('/article/{id}', name: 'article_show', methods: ['GET'])]
-    
-    public function show(ArticleRepository $articles, $id): Response{
-        return $this->render('article/show.html.twig', [
-            'articles' => $articles->findOneBy(
-                    [id => $id],
-                )
-            ]);
-        
-    }
+    #[Route('/article/{id}', name: 'app_article_show', methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        $article = $this->articleRepository->find($id);
 
-    
+        if (!$article) {
+            throw $this->createNotFoundException('L\'article demandé n\'existe pas');
+        }
+
+        return $this->render('article/show.html.twig', [
+            'article' => $article
+        ]);
+    }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,22 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    public function __construct(private ArticleRepository $articleRepository)
+    {
+    }
+
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $recentArticles = $this->articleRepository->findRecentArticles(6);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'articles' => $articles->findBy(
-                [],
-                ['createdAt' => 'DESC'],
-                6
-            )
+            'articles' => $recentArticles
         ]);
     }
 }
